7188c10150bb1c5290488359c74f9d4ead063dae
Nathan_Swinger <nathanaswinger@gmail.com>
2024-02-02 13:15:12 -0600

    Project Init

---
+++ src/main/java/com/msu/ctap/CtapApplication.java
@@ -0,0 +1,13 @@
+package com.msu.ctap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msu/ctap/CtapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msu.ctap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
